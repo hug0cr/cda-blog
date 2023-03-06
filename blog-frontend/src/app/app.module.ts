1bed1d595ee46c32511536ceb24dfbbf5aa50b73
hug0cr <pro.hugocamacho@gmail.com>
2023-03-06 11:48:43 +0100

    Dumb Home page

--- blog-frontend/src/app/app.module.ts
+++ blog-frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { PresentationComponent } from './pages/home/presentation/presentation.component';
+import { ArticlesComponent } from './pages/home/articles/articles.component';
+import { LastArticlesComponent } from './pages/home/articles/last-articles/last-articles.component';
+import { PopularArticlesComponent } from './pages/home/articles/popular-articles/popular-articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    PresentationComponent,
+    ArticlesComponent,
+    LastArticlesComponent,
+    PopularArticlesComponent
   ],
   imports: [
     BrowserModule,
---
+++ blog-frontend/src/app/pages/home/articles/popular-articles/popular-articles.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {Article} from "../../../../shared/model/article";
+
+@Component({
+  selector: 'app-popular-articles',
+  templateUrl: './popular-articles.component.html',
+  styleUrls: ['./popular-articles.component.scss']
+})
+export class PopularArticlesComponent {
+  popularArticles: Article[] = [
+    {
+      id: 1,
+      title: 'Spring',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris faucibus felis sodales nibh porta suscipit. Proin sit amet est vitae leo scelerisque volutpat. Vivamus imperdiet, tellus eu fringilla pharetra, ante quam venenatis dolor, quis ultricies mi velit at arcu.'
+    },
+    {
+      id: 2,
+      title: 'Angular',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris faucibus felis sodales nibh porta suscipit. Proin sit amet est vitae leo scelerisque volutpat. Vivamus imperdiet, tellus eu fringilla pharetra, ante quam venenatis dolor, quis ultricies mi velit at arcu.'
+    }
+  ];
+}
