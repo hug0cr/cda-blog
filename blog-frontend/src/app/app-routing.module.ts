36283501f93122246d12944353da7dadc9797b37
hug0cr <pro.hugocamacho@gmail.com>
2023-03-07 17:07:55 +0100

    Refactoring ui

--- blog-frontend/src/app/app-routing.module.ts
+++ blog-frontend/src/app/app-routing.module.ts
@@ -4,12 +4,34 @@ import {HomeComponent} from "./blog/home/home.component";
 import {CreateArticleComponent} from "./blog/create-article/create-article.component";
 import {PageNotFoundComponent} from "./blog/page-not-found/page-not-found.component";
 import {AuthGuard} from "./core/auth.guard";
+import {BlogComponent} from "./blog/blog.component";
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'create-article', component: CreateArticleComponent, canActivate: [AuthGuard]},
-  {path: 'page-not-found', component: PageNotFoundComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {
+    path: '',
+    component: BlogComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'create-article',
+        component: CreateArticleComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'page-not-found',
+        component: PageNotFoundComponent
+      },
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+    ]
+  },
+
   {path: '**', redirectTo: "/page-not-found"},
 ];
 
--- blog-frontend/src/app/app.module.ts
+++ blog-frontend/src/app/app.module.ts
@@ -4,8 +4,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './blog/home/home.component';
-import {FooterComponent} from './shared/components/footer/footer.component';
-import {HeaderComponent} from './shared/components/header/header.component';
+import {FooterComponent} from './blog/ui/footer/footer.component';
+import {HeaderComponent} from './blog/ui/header/header.component';
 import {PresentationComponent} from './blog/home/presentation/presentation.component';
 import {ArticlesComponent} from './blog/home/articles/articles.component';
 import {LastArticlesComponent} from './blog/home/articles/last-articles/last-articles.component';
@@ -15,6 +15,7 @@ import {PageNotFoundComponent} from './blog/page-not-found/page-not-found.compon
 import {initializeKeycloak} from "./core/keycloak-init.factory";
 import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
 import {HttpClientModule} from "@angular/common/http";
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import {HttpClientModule} from "@angular/common/http";
     PopularArticlesComponent,
     CreateArticleComponent,
     PageNotFoundComponent,
+    BlogComponent,
   ],
   imports: [
     BrowserModule,
